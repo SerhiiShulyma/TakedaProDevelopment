fc0a204879e3dac958c51f1c5d7163474df67b28
Serhii <ser.shulyma@gmail.com>
2019-09-30 02:07:23 +0300

    Creating dropdown menu for landings (mobile and desktop)

---
+++ web/themes/custom/new_structure_takeda/js/dropdown-landing.js
@@ -0,0 +1,15 @@
+(function($, Drupal){
+    Drupal.behaviors.dropdownForLandings={
+        attach: function(context, settings){
+            
+            $('.landing-title').click(function(){
+                $(this).parent().find('.landing-sites').toggleClass('closed');
+/*                var  blockHeight = $(this).parent().height();
+                console.log(blockHeight);*/
+               /* if(! ($('.takeda-sites').find('.closed').length==1)){
+                   $('.landing').toggleClass('open');
+                } */
+            });            
+        }
+    };
+})(jQuery, Drupal);
