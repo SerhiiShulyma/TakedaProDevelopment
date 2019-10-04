fcc27ecdae2bcc2c1021fcfe8dcb906850bc5b9e
Serhii <ser.shulyma@gmail.com>
2019-10-04 18:20:24 +0300

    Default open dropdown menu

--- web/themes/custom/new_structure_takeda/js/dropdown-landing.js
+++ web/themes/custom/new_structure_takeda/js/dropdown-landing.js
@@ -1,16 +1,17 @@
 (function($, Drupal){
     Drupal.behaviors.dropdownForLandings={
         attach: function(context, settings){
-            
             $('.landing-title').click(function(){
                 $(this).parent().find('.landing-sites').toggleClass('closed');
                 $(this).toggleClass('active-landing');
-/*                var  blockHeight = $(this).parent().height();
-                console.log(blockHeight);*/
-               /* if(! ($('.takeda-sites').find('.closed').length==1)){
-                   $('.landing').toggleClass('open');
-                } */
-            });            
+
+            //// Adding white area when dropdown menu open    
+                 if( ($('.landing-title').hasClass('active-landing'))){
+                     $('#block-choose-a-therapeutic-area').height (300);
+                 } else {
+                     $('#block-choose-a-therapeutic-area').height(178);
+                 };    
+            });    
         }
     };
 })(jQuery, Drupal);
