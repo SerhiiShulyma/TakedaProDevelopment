b79018d60c98e2a58f40199a3a3412e00bfa9bd0
Ефим Станкевич <yephim@gmail.com>
2019-10-07 12:36:42 +0300

    tablet

--- web/themes/custom/new_structure_takeda/js/dropdown-landing.js
+++ web/themes/custom/new_structure_takeda/js/dropdown-landing.js
@@ -4,13 +4,15 @@
             $('.landing-title').click(function(){
                 $(this).parent().find('.landing-sites').toggleClass('closed');
                 $(this).toggleClass('active-landing');
+                var rel = $(this).attr('rel-data');
+                $('#'+rel).toggleClass('open');
 
-            //// Adding white area when dropdown menu open    
-                 if( ($('.landing-title').hasClass('active-landing'))){
+              //// Adding white area when dropdown menu open
+                /* if( ($('.landing-title').hasClass('active-landing'))){
                      $('#block-choose-a-therapeutic-area').height (300);
                  } else {
                      $('#block-choose-a-therapeutic-area').height(178);
-                 };    
+                 }; */
             });    
         }
     };
